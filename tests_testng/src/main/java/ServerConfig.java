a8b274e73fb2bf61e518a79f9cdc8b1c0ef7287d
nprokazova <naprokazova@gmail.com>
2021-04-11 22:31:19 +0300

    create project

---
+++ tests_testng/src/main/java/ServerConfig.java
@@ -0,0 +1,9 @@
+import org.aeonbits.owner.Config;
+
+public interface ServerConfig extends Config {
+    int port();
+    String hostname();
+    @DefaultValue("42")
+    int maxThreads();
+
+}
---
+++ tests_testng/src/test/java/Lesson3.java
@@ -0,0 +1,51 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Lesson3 {
+
+    private Logger logger = LogManager.getLogger(Lesson3.class);
+    protected static WebDriver driver;
+
+    @BeforeTest
+    public void StartUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
+    }
+
+    @AfterTest
+    public void End(){
+        if (driver!=null)
+            driver.quit();
+    }
+
+    @Test
+    public void LogExample(){
+        ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+
+        logger.info("Start test: info level");
+        logger.error("Start test: error level");
+        logger.warn("Start test: warm level");
+        logger.fatal("Start test: fatal level");
+        logger.debug("Start test: debug level");
+        logger.trace("Start test: trace level");
+
+        System.out.println("Server " + cfg.hostname() + ":" + cfg.port() +
+                " will run " + cfg.maxThreads());
+    }
+
+    @Test
+    public void WebDriverTest(){
+        driver.get("https://otus.ru");
+        logger.info("Сайт открыт");
+
+    }
+
+}
